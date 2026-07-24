3edb51612d580fd29e72e02267c7e37cf0e7ee44
dazhu12300-sudo <dazhu12300@gmail.com>
2026-07-24 20:49:16 +0300

    Update contact-config.js

--- contact-config.js
+++ contact-config.js
@@ -2,7 +2,7 @@
 var CONTACT_CONFIG = {
     hours: "9h – 21h (UTC+3) ｜ በኢትዮጵያ አቆጣጠር፡ ከጠዋቱ 3፡00 - ከምሽቱ 3፡00",
     contacts: [
-        { name: "Jessica Telegram",   url: "https://t.me/PGJessica",      icon: "📱" },
-        { name: "Jessica Telegram",  url: "https://t.me/PG91Jessica91",       icon: "📱" },
+        { name: "Jessica(Main) Telegram",   url: "https://t.me/PGJessica",      icon: "📱" },
+        { name: "Jessica(Backup) Telegram",  url: "https://t.me/PG91Jessica91",       icon: "📱" },
     ]
 };
